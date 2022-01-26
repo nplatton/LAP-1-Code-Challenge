85f51e41a10ec581bc90fc0efc372728caf577c8
Noah Platton <noahplatton@gmail.com>
2022-01-26 15:05:18 +0000

    Finish database

--- server/data.js
+++ server/data.js
@@ -1,7 +1,7 @@
 let database = {
   cat: [
     ['Cat - Wikipedia',
-      'The earliest known indication for the taming of an African wildcat (F. lybica) was excavated close by a human Neolithic grave in Shillourokambos, southern Cyprus, dating to about 7500–7200 BC. Since there is no evidence of native mammalian fauna on Cyprus, the inhabitants of this Neolithic village most likely brought the cat and other wild mammals to the island from the Middle Eastern mainland.[46] Scientists therefore assume that African wildcats were attracted to early human settlements in the Fertile Crescent by rodents, in particular the house mouse (Mus musculus), and were tamed by Neolithic farmers. This commensal relationship between early farmers and tamed cats lasted thousands of years. As agricultural practices spread, so did tame and domesticated cats.[11][6] Wildcats of Egypt contributed to the maternal gene pool of the domestic cat at a later time.',
+      'The earliest known indication for the taming of an African wildcat (F. lybica) was excavated close by a human Neolithic grave in Shillourokambos, southern Cyprus, dating to about 7500–7200 BC. Since there is no evidence of native mammalian fauna on Cyprus, the inhabitants of this Neolithic village most likely brought the cat and other wild mammals to the island from the Middle Eastern mainland. Scientists therefore assume that African wildcats were attracted to early human settlements in the Fertile Crescent by rodents, in particular the house mouse (Mus musculus), and were tamed by Neolithic farmers. This commensal relationship between early farmers and tamed cats lasted thousands of years. As agricultural practices spread, so did tame and domesticated cats.[11][6] Wildcats of Egypt contributed to the maternal gene pool of the domestic cat at a later time.',
       'https://en.wikipedia.org/wiki/Cat'
     ],
     ['Choosing a Cat - Making the Right Choice for You',
@@ -49,10 +49,60 @@ let database = {
 
   ],
   rabbit: [
-
+    ['Rabbit - Wikepedia',
+    "Rabbits, also known as bunnies or bunny rabbits, are small mammals in the family Leporidae (along with the hare) of the order Lagomorpha (along with the pika). Oryctolagus cuniculus includes the European rabbit species and its descendants, the world's 305 breeds of domestic rabbit. Sylvilagus includes 13 wild rabbit species, among them the seven types of cottontail. The European rabbit, which has been introduced on every continent except Antarctica, is familiar throughout the world as a wild prey animal and as a domesticated form of livestock and pet. With its widespread effect on ecologies and cultures, the rabbit is, in many areas of the world, a part of daily life—as food, clothing, a companion, and a source of artistic inspiration.",
+    'https://en.wikipedia.org/wiki/Rabbit'
+    ],
+    ['rabbit | description, species, & facts | britannica',
+      'Frequently the terms rabbit and hare are used interchangeably, a practice that can cause confusion. Jackrabbits, for instance, are actually hares, whereas the rockhares and the hispid hare are rabbits.',
+      'https://www.britannica.com/animal/rabbit'
+    ],
+    ['species - rabbit - the mammal society',
+      'There are a number of reports now indicating that rabbits were originally brought over by the Romans in the 1st century. What remains unclear is when a wild breeding population was established after this. Rabbits are now widespread throughout Britain and Ireland, but are absent from Rum, Isles of Scilly and a few smaller islands.',
+      'https://www.mammal.org.uk/species-hub/full-species-hub/discover-mammals/species-rabbit/'
+    ],
+    ['rabbit supplies | hutches, food & toys | pets at home',
+      'Looking for the perfect rabbit products to make sure your bunny feels right at home? Our rabbit supplies range from bowls to bedding with hutches, treats, hygiene product and much more in-between.',
+      'https://www.petsathome.com/shop/en/pets/small-pet/rabbit'
+    ],
+    ['rabbit restaurant',
+    "Rabbit is on the iconic Kings Road, Chelsea. It is a farm-to-fork neighbourhood restaurant focusing on sustainability, capturing the English countryside's flavours, freshness, and vibrancy on this famous London Street. Rabbit serves modern British dishes with knowledgeable, attentive service and an electric atmosphere.",
+    'https://www.rabbit-restaurant.com/'
+    ],
+    ['keeping rabbits as pets | rspca',
+      "Rabbits are a popular choice for many families, with an estimated 900,000 rabbits kept as pets in the UK. And it's no surprise, as rabbits are highly intelligent, inquisitive animals. Owning rabbits can be extremely rewarding.",
+      'https://www.rspca.org.uk/adviceandwelfare/pets/rabbits'
+    ],
+    ['rabbit | the wildlife trusts',
+    'Who doesn’t love spotting rabbits hopping through long grass during a walk in the countryside? They are a common sight but it is always a treat to see their curious faces popping up, ears stood tall on the look out for predators.',
+    'https://www.wildlifetrusts.org/wildlife-explorer/mammals/rabbit'
+    ],
+    ['rabbit animal facts | oryctolagus cuniculus',
+    'Rabbits really do breed like rabbits. The female is ready to breed almost any time, and she’ll have a litter of babies about 30 days after breeding. These herbivores eat a diet of mostly green foods, but they are also opportunistic feeders that will eat seeds, fruit, and bark.',
+    'https://a-z-animals.com/animals/rabbit/'
+    ],
+    ['welcome to the official home of peter rabbit',
+    "Welcome to the official home of Peter Rabbit and his friends. Here you'll find news, information and activities to delight little bunnies.",
+      'https://www.peterrabbit.com/'
+    ],
+    ['asuitable environment for rabbits | nidirect',
+     'A rabbit’s environment is where it lives - not just where it sleeps, eats, exercises and goes to the toilet, but any place that it has access to. The environment also includes all the objects a rabbit comes into contact with and the materials, such as bedding, that it needs to stay healthy and happy.',
+     'https://www.nidirect.gov.uk/articles/suitable-environment-rabbits'
+    ]
   ],
   ostrich: [
-
+    ['ostrich - wikipedia',
+      'The earliest fossils of ostrich-like birds are Paleocene taxa from Europe. Palaeotis and Remiornis from the Middle Eocene and unspecified ratite remains are known from the Eocene and Oligocene of Europe and Africa.',
+      'https://en.wikipedia.org/wiki/Ostrich'
+    ],
+    ['ostrich | national geographic',
+      'Though they cannot fly, ostriches are fleet, strong runners. They can sprint up to 43 miles an hour and run over distance at 31 miles an hour. They may use their wings as "rudders" to help them change direction while running.',
+      'https://www.nationalgeographic.com/animals/birds/facts/ostrich'
+    ],
+    ['ostrich | san diego zoo animals & plants',
+      'As it is so heavy, this flightless bird that can never take to the skies; instead, it’s built to run. Its long, thick, and powerful legs can cover great distances without much effort, and its feet have only two toes for greater speed.',
+      'https://animals.sandiegozoo.org/animals/ostrich'
+    ]
   ]
 }
 
