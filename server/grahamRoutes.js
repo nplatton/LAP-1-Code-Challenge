e708345757c31c6fedf287d3e0c30e8f48a04cf2
“ewhite1999” <ewhite1999@gmail.com>
2022-01-26 14:45:20 +0000

    fetch listener

--- server/grahamRoutes.js
+++ server/grahamRoutes.js
@@ -1,25 +1,25 @@
-const { pageFetcher } = require('./serverHelpers')
+const { pageFetcher } = require('./serverHelpers');
 
-const express = require('express')
-const router = express.Router()
+const express = require('express');
+const router = express.Router();
 
 router.get('/', (req, res) => {
-    res.json('Welcome to Graham!')
-})
+  res.json('Welcome to Graham!');
+});
 
 router.get('/:animal', (req, res) => {
-    try {
-        let animal = req.params.animal
-        let formatted = animal.split(/[\W_]/g).join('%20')
-        let url = `https://www.google.co.uk/search?q=${formatted}`
+  try {
+    let animal = req.params.animal;
+    let formatted = animal.split(/[\W_]/g).join('%20');
+    let url = `https://www.google.co.uk/search?q=${formatted}`;
 
-        // response = pageFetcher(url)
-        // res.json(response)
+    // response = pageFetcher(url)
+    // res.json(response)
 
-        res.json(url)
-    } catch(err) {
-        res.status(404).json(err.message)
-    }
-})
+    res.json(url);
+  } catch (err) {
+    res.status(404).json(err.message);
+  }
+});
 
 module.exports = router;
