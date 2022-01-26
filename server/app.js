a51beca66bc1de2263a0921083f0cb6523c35362
Noah Platton <noahplatton@gmail.com>
2022-01-26 11:10:11 +0000

    Create graham routes

--- server/app.js
+++ server/app.js
@@ -5,10 +5,11 @@ const cors = require('cors')
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.json('Welcome to Graham!')
-})
+// app.get('/', (req, res) => {
+//     res.json('Welcome to Graham!')
+// })
 
-
+const grahamRoutes = require('./grahamRoutes')
+app.use('/graham', grahamRoutes);
 
 module.exports = { app };
