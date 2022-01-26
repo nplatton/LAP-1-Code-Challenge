3f909c88c8bde3f73cfdfde2d92313cc50e3ff06
“ewhite1999” <ewhite1999@gmail.com>
2022-01-26 15:02:51 +0000

    dog data

--- server/data.js
+++ server/data.js
@@ -1,59 +1,115 @@
 let database = {
   cat: [
-    ['Cat - Wikipedia',
+    [
+      'Cat - Wikipedia',
       'The earliest known indication for the taming of an African wildcat (F. lybica) was excavated close by a human Neolithic grave in Shillourokambos, southern Cyprus, dating to about 7500–7200 BC. Since there is no evidence of native mammalian fauna on Cyprus, the inhabitants of this Neolithic village most likely brought the cat and other wild mammals to the island from the Middle Eastern mainland.[46] Scientists therefore assume that African wildcats were attracted to early human settlements in the Fertile Crescent by rodents, in particular the house mouse (Mus musculus), and were tamed by Neolithic farmers. This commensal relationship between early farmers and tamed cats lasted thousands of years. As agricultural practices spread, so did tame and domesticated cats.[11][6] Wildcats of Egypt contributed to the maternal gene pool of the domestic cat at a later time.',
-      'https://en.wikipedia.org/wiki/Cat'
+      'https://en.wikipedia.org/wiki/Cat',
     ],
-    ['Choosing a Cat - Making the Right Choice for You',
+    [
+      'Choosing a Cat - Making the Right Choice for You',
       'Whether you’re a lifelong cat-lover or embracing the idea of a new pet, getting a new cat or kitten is a great experience. At Cats Protection, we believe in matching the right cat to the right home. After all, just like people, all cats are different.',
-      'https://www.cats.org.uk/adopt-a-cat/preparing-for-a-cat'
+      'https://www.cats.org.uk/adopt-a-cat/preparing-for-a-cat',
     ],
-    ['Cat | National Geographic',
-    'Like their wild relatives, domestic cats are natural hunters able to stalk prey and pounce with sharp claws and teeth. They are particularly effective at night, when their light-reflecting eyes allow them to see better than much of their prey. Cats also enjoy acute hearing. All cats are nimble and agile, and their long tails aid their outstanding balance.',
-    'https://www.nationalgeographic.com/animals/mammals/facts/domestic-cat'
+    [
+      'Cat | National Geographic',
+      'Like their wild relatives, domestic cats are natural hunters able to stalk prey and pounce with sharp claws and teeth. They are particularly effective at night, when their light-reflecting eyes allow them to see better than much of their prey. Cats also enjoy acute hearing. All cats are nimble and agile, and their long tails aid their outstanding balance.',
+      'https://www.nationalgeographic.com/animals/mammals/facts/domestic-cat',
     ],
-    ['cat | Breeds & Facts | Britannica',
-     'The “cat pattern,” established very early in the evolution of modern mammals, was a successful one: early cats were already typical in form at a time when the ancestors of most other modern mammalian types were scarcely recognizable. They first appeared in the early Pliocene Epoch (5.3 to 3.6 million years ago), and they have continued with remarkably little change into modern times.',
-     'https://www.britannica.com/animal/cat' 
+    [
+      'cat | Breeds & Facts | Britannica',
+      'The “cat pattern,” established very early in the evolution of modern mammals, was a successful one: early cats were already typical in form at a time when the ancestors of most other modern mammalian types were scarcely recognizable. They first appeared in the early Pliocene Epoch (5.3 to 3.6 million years ago), and they have continued with remarkably little change into modern times.',
+      'https://www.britannica.com/animal/cat',
     ],
-    ['List of Cat Breeds | Petfinder',
-    'Browse our list of 57 cat breeds to find the perfect cat breed for you, and then find adoptable cats and cat shelters close to you.',
-    'https://www.petfinder.com/cat-breeds/'
+    [
+      'List of Cat Breeds | Petfinder',
+      'Browse our list of 57 cat breeds to find the perfect cat breed for you, and then find adoptable cats and cat shelters close to you.',
+      'https://www.petfinder.com/cat-breeds/',
     ],
-    ['Cat Care Tips, Advice & Health Information | RSPCA',
-    "Cats make affectionate and playful pets and thrive best when they're cared for considering their dietary, environmental and health and welfare needs. Cats will need space to be active, with regular interaction with people and other animals (cats don't really like to be left alone for long periods during the day). They will also need access to clean water, meat-based cat food and regular grooming as well as private access to a litter tray which should ideally be replaced once a week.",
-    'https://www.rspca.org.uk/adviceandwelfare/pets/cats'
+    [
+      'Cat Care Tips, Advice & Health Information | RSPCA',
+      "Cats make affectionate and playful pets and thrive best when they're cared for considering their dietary, environmental and health and welfare needs. Cats will need space to be active, with regular interaction with people and other animals (cats don't really like to be left alone for long periods during the day). They will also need access to clean water, meat-based cat food and regular grooming as well as private access to a litter tray which should ideally be replaced once a week.",
+      'https://www.rspca.org.uk/adviceandwelfare/pets/cats',
     ],
-    ['Cat Care | Grooming | Nutrition | Disease | Behaviour | ASPCA',
-    'Do you have a feline companion? We’ve got you covered. Our ASPCA veterinarians and behaviorists offer up tips, solutions and answers to some of your most frequently asked feline questions to keep your kitty happy and healthy.',
-    'https://www.aspca.org/pet-care/cat-care'
+    [
+      'Cat Care | Grooming | Nutrition | Disease | Behaviour | ASPCA',
+      'Do you have a feline companion? We’ve got you covered. Our ASPCA veterinarians and behaviorists offer up tips, solutions and answers to some of your most frequently asked feline questions to keep your kitty happy and healthy.',
+      'https://www.aspca.org/pet-care/cat-care',
     ],
-    ['Cat Supplies | Food, Toys, Collars & Care | Pets At Home',
-    'Pets at Home has a wide range of cat supplies to suit any cat; take a look at our selection of food and treats or browse our other cat products to see everything on offer!',
-    'https://www.petsathome.com/shop/en/pets/cat#'
+    [
+      'Cat Supplies | Food, Toys, Collars & Care | Pets At Home',
+      'Pets at Home has a wide range of cat supplies to suit any cat; take a look at our selection of food and treats or browse our other cat products to see everything on offer!',
+      'https://www.petsathome.com/shop/en/pets/cat#',
     ],
-    ['Cats - News, views, gossip, pictures, video - The Mirror',
-    'The owners of an overweight cat have hit back at online trolls claiming the enormous kitty is "on a diet" after many shared their concerns at her large...',
-    'https://www.mirror.co.uk/all-about/cats'
+    [
+      'Cats - News, views, gossip, pictures, video - The Mirror',
+      'The owners of an overweight cat have hit back at online trolls claiming the enormous kitty is "on a diet" after many shared their concerns at her large...',
+      'https://www.mirror.co.uk/all-about/cats',
     ],
-    ['Complete Guide to Caring for Cats - Vetstreet',
-    "Your cat's online owners manual, featuring articles about breed information, cat selection, training, grooming and care for cats and kittens.",
-    'http://www.vetstreet.com/cats/'
+    [
+      'Complete Guide to Caring for Cats - Vetstreet',
+      "Your cat's online owners manual, featuring articles about breed information, cat selection, training, grooming and care for cats and kittens.",
+      'http://www.vetstreet.com/cats/',
     ],
-    ['Adopt a Cat | Blue Cross',
-    'Meet the cats and kittens currently waiting to be adopted. You can find out more by clicking on their profile. We’ll explain what they’re like and what they might need from a new home. If you think you could offer a cat the love they deserve, fill in an application form to express your interest.',
-    'https://www.bluecross.org.uk/rehome/cat'
-    ]
+    [
+      'Adopt a Cat | Blue Cross',
+      'Meet the cats and kittens currently waiting to be adopted. You can find out more by clicking on their profile. We’ll explain what they’re like and what they might need from a new home. If you think you could offer a cat the love they deserve, fill in an application form to express your interest.',
+      'https://www.bluecross.org.uk/rehome/cat',
+    ],
   ],
   dog: [
-
+    [
+      'Dogs trust',
+      'I feel lost and alone. Will you pick me up? Help us care for stray dogs starting the New Year without a home.Right now more owners are struggling with the impact of the pandemic, as well as rising costs of living during these next few months. Unfortunately, this makes it even more likely that dogs of any age or condition will find themselves without a home, a family, or any protection.',
+      'https://www.dogstrust.org.uk/',
+    ],
+    [
+      'Dog - Wikipedia',
+      "The dog or domestic dog (Canis familiaris or Canis lupus familiaris) is a domesticated descendant of the wolf which is characterized by an upturning tail. The dog derived from an ancient, extinct wolf, and the modern grey wolf is the dog's nearest living relative. The dog was the first species to be domesticated, by hunter–gatherers over 15,000 years ago, before the development of agriculture",
+      'https://en.wikipedia.org/wiki/Dog',
+    ],
+    [
+      'Dog Care Advice, Tips & Health Information | RSPCA',
+      'Owning a dog is great fun and immensely rewarding. But given that dogs have complex needs, our expert advice will help you ensure your dog is healthy and happy. Take a look around and learn more about caring for a dog!',
+      'https://www.rspca.org.uk/adviceandwelfare/pets/dogs',
+    ],
+    [
+      'Dog, facts and photos - National Geographic',
+      'The term “domestic dog” refers to any of several hundred breeds of dog in the world today. While these animals vary drastically in appearance, every dog—from the Chihuahua to the Great Dane—is a member of the same species, Canis familiaris. This separates domestic dogs from wild canines, such as coyotes, foxes, and wolves.',
+      'https://www.nationalgeographic.com/animals/mammals/facts/domestic-dog',
+    ],
+    [
+      'All Dog Breeds - Complete List of Dog Profiles - DogTime',
+      'The Afador is a mixed breed dog–a cross between the Afghan Hound and Labrador Retriever dog breeds. Loyal, energetic, and affectionate, these pups inherited some of the best qualities from both of their parents.',
+      'https://dogtime.com/dog-breeds/profiles',
+    ],
+    [
+      'Dogs | Healthy Pets, Healthy People | CDC',
+      'Dogs can have many positive effects on the lives of their owners. They influence social, emotional, and cognitive development in children, promote an active lifestyle, provide companionship, and have even been able to detect oncoming epileptic seizures or the presence of certain cancers. Dogs can also help to relieve stress and anxiety in people. Around 38% of US households are estimated to have one or more dogs.',
+      'https://www.cdc.gov/healthypets/pets/dogs.html',
+    ],
+    [
+      'Dogs for sale in Bedford, Bedfordshire - Gumtree',
+      "It's extremely unfortunate that we have had to make the sad decision to rehome our sweet beautiful boy.Yogi is a 10 month old Springer x Beagle puppy. He has been microchipped, is fully vaccinated &amp; neutered and has been health checked regularly",
+      'https://www.gumtree.com/dogs/bedford',
+    ],
+    [
+      'Most Popular Dog Breeds - Good Housekeeping',
+      "If you are wanting to cuddle up on the couch with an adorable and furry friend but have a difficult time with allergies, then a hypoallergenic dog might just be the perfect answer for all your snuggles. So what makes hypoallergenic dogs great for people with allergies? One main plus to hypoallergenic dogs is that they don’t shed, meaning there's little dander coming off their coats. Plus, since they don’t shed, you don’t have to constantly be picking up or vacuuming stubborn hair around the house. ",
+      'https://www.goodhousekeeping.com/dog-breeds/',
+    ],
+    [
+      'Dogs | Lifeandstyle | The Guardian',
+      'A lot of people in the US got a dog in the pandemic. Almost 9 million dogs have been acquired by households since the start of the pandemic (a number equivalent to the population of New York City) bringing the total number of dogs in the US to 108 million (that’s roughly one dog to every three humans in the US). And cats crept up too – an additional 5 million in the space of just a few years bringing the total US feline count to 79 million.',
+      'https://www.theguardian.com/lifeandstyle/dogs',
+    ],
+    [
+      'Complete Guide to Caring for Dogs - Vetstreet.com',
+      "The pet-owning population is becoming more and more educated on the facts about dog behavior, but there are still a number of dog behavior myths that persist. And those myths aren't just annoying for trainers like me — they can also be detrimental to your relationship with your dog!",
+      'http://www.vetstreet.com/dogs/',
+    ],
   ],
-  rabbit: [
-
-  ],
-  ostrich: [
-
-  ]
-}
+  rabbit: [],
+  ostrich: [],
+};
 
 module.exports = database;
